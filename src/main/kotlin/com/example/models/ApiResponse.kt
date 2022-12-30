275770f8d3c559d614efd06254b62e76938f2a77
Dario DAngelo <dariodangelo21@gmail.com>
2022-12-30 16:48:19 -0300

    quit lastUpdated

--- src/main/kotlin/com/example/models/ApiResponse.kt
+++ src/main/kotlin/com/example/models/ApiResponse.kt
@@ -8,6 +8,5 @@ data class ApiResponse(
     val message: String? = null,
     val prevPage: Int? = null,
     val nextPage: Int? = null,
-    val books: List<Book> = emptyList(),
-    val lastUpdated: Long? = null
+    val books: List<Book> = emptyList()
 )
